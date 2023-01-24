d8df0b045d5c18c216fe1101331f9b112080e24d
Eugene Lazutin <eugenelazutin@gmail.com>
2023-01-24 19:28:22 +0100

    configured eslint and prettier

--- routes/api.ts
+++ routes/api.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express';
-var router = express.Router();
+import express, { Request, Response } from "express";
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req: Request, res: Response) {
-    res.send({ message: 'respond with a resource' });
+router.get("/", (req: Request, res: Response) => {
+  res.send({ message: "respond with a resource" });
 });
 
 module.exports = router;
